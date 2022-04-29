226792df9f06c3b8e71a4f8f94dc7a8811c35656
cprob-repo <47336567+cprob-repo@users.noreply.github.com>
2022-04-29 23:11:39 +0000

    Add files via upload

---
+++ CanSat/Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace CanSat
+{
+    public class Controller
+    {
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new View());
+        }
+
+        
+    }
+}
